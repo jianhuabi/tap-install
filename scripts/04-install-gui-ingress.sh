03912faea37c8f4cad6618c32add7ed58dda30f6
michaelbi <jianhua.bi@hotmail.com>
2022-01-21 16:36:42 +0800

    made no tls as default

--- scripts/04-install-gui-ingress.sh
+++ scripts/04-install-gui-ingress.sh
@@ -4,6 +4,14 @@
 # install GUI Ingress via Contour httpproxy API
 kubectl create ns tanzu-system-ingress
 
-ytt --ignore-unknown-comments -f values.yaml -f ingress-config/ | kubectl apply -f- 
+# install ingress httpproxy
+ytt --ignore-unknown-comments -f values.yaml -f ingress-config/api-portal-ingress.yaml | kubectl apply -f- 
+ytt --ignore-unknown-comments -f values.yaml -f ingress-config/metadata-store-ingress.yaml | kubectl apply -f- 
+
+# install certificate is optional
+# ytt --ignore-unknown-comments -f values.yaml -f ingress-config/certificate.yaml | kubectl apply -f- 
+# ytt --ignore-unknown-comments -f values.yaml -f ingress-config/cnr-certificate.yaml | kubectl apply -f- 
+# ytt --ignore-unknown-comments -f values.yaml -f ingress-config/lets-encrypt-cluster-issuer.yaml | kubectl apply -f- 
+# ytt --ignore-unknown-comments -f values.yaml -f ingress-config/ltls-cert-delegation.yaml | kubectl apply -f- 
 
 # ytt --ignore-unknown-comments -f values.yaml -f external-dns/ | kubectl apply -f- -n tanzu-system-ingress
